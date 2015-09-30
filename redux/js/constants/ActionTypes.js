ba517a77ed4733ea28e12da8a8e09ef95e66672a
Francis Devereux <francis@bright-interactive.co.uk>
2015-09-30 10:19:11 +0100

    Add missing ActionType constant
    Used by ActionCreators.getAllProducts().
    
    Fixes 'Actions may not have an undefined "type" property' error with Redux 3.

--- redux/js/constants/ActionTypes.js
+++ redux/js/constants/ActionTypes.js
@@ -1,6 +1,7 @@
 import keyMirror from 'react/lib/keyMirror';
 
 export default keyMirror({
+    GET_ALL_PRODUCTS: null,
     RECEIVE_PRODUCTS: null,
     ADD_TO_CART: null,
     BEGIN_CHECKOUT: null,
